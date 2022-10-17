d1f209fc450d1d2529ce6daaf243109949c95ce5
Nosoir <noeliairace13@gmail.com>
2022-10-17 16:05:34 +0000

    Simple Counter with React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+  
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+        <div className="d-flex justify-content-center">
+			<div className="border border-secondary rounded align-self-center p-2 m-1 bg-secondary text-light">
+				<i className="far fa-clock fa-2x"></i>
+			</div>
+			<div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.hora_izquierda}</h1>
+			</div>
+			<div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.hora_derecha}</h1>
+			</div>
+			<div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.minutos_izquierda}</h1>
+			</div>
+			<div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.minutos_derecha}</h1>
+			</div>
+			<div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.segundos_izquierda}</h1>
+			</div>
+            <div className="border border-secondary rounded px-2 m-1 align-self-center bg-secondary text-light">
+				<h1>{props.segundos_derecha}</h1>
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,42 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seg_derecha = 0;
+let seg_izquierda = 0;
+let min_derecha = 0;
+let min_izquierda = 0;
+let hor_izquierda = 0;
+let hor_derecha = 0;
+
+setInterval(()=>{
+    seg_derecha++;
+    if (seg_derecha > 9) {
+        seg_derecha = 0
+        seg_izquierda++
+        if (seg_izquierda > 5) {
+            seg_izquierda = 0
+            min_derecha++
+            if (min_derecha > 9) {
+                min_derecha = 0
+                min_izquierda++
+                if (min_izquierda > 5) {
+                    min_izquierda = 0
+                    hor_derecha++
+                    if (hor_derecha > 9) {
+                        hor_derecha = 0
+                        hor_izquierda++
+                    }
+                }
+            }
+        }
+    }
+    ReactDOM.render(<Home 
+                        segundos_derecha={seg_derecha} 
+                        segundos_izquierda={seg_izquierda}
+                        minutos_derecha = {min_derecha}
+                        minutos_izquierda = {min_izquierda}
+                        hora_derecha = {hor_derecha}
+                        hora_izquierda = {hor_izquierda}
+                        />, 
+                        document.querySelector("#app"));
+},1000)
